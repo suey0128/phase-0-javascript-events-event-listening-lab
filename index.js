f14ec3c195b9b4d984509e2399407d47e9ae3f13
suey0128 <syui0128@gmail.com>
2021-04-28 13:01:29 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+
+const input = getElementById('input')
+
+/*
 function addingEventListener() {
+    input.addEventListener('click',function(){
+        alert('I was clicked!');
+    })
 }
+
+*/
+
+
+function addingEventListener (){
+    function enAlert (){
+        alert('I was clicked!');
+    }
+    input.addEventListener('click', enAlert);
+}
+
